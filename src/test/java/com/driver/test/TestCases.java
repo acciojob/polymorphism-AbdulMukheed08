498e779202bc73b5db9762d463ef8c6f48f86e8b
Abdul Mukheed <you@example.com>
2023-01-22 14:12:49 +0530

    Polymorphism 4

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -6,3 +6,15 @@ import org.junit.jupiter.api.Test;
 public class TestCases {
      
 }
+class Product {
+    public int product(int x,int y){
+        return x*y;
+    }
+    public int product(int x,int y,int z){
+        return x*y*z;
+    }
+    public double product(double x,double y){
+        return x*y;
+    }
+}
+
